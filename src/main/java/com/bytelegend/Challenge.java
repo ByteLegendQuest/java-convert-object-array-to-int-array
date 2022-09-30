2d484a64cd398547aa6d6510a56a5be6df8c58b4
lhhhhhhhhhhhhhh <lihan881212@gmail.com>
2022-09-30 15:51:21 +0800

    111

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+
 public class Challenge {
     /**
      * Given an array containing any objects `Object[]`, convert it to `int[]` array. The objects
@@ -13,6 +15,22 @@ public class Challenge {
      * <p>For example, input `[1, 2.0, 3.1, "4.999"]`, the output is `[1, 2, 3, 4]`.
      */
     public static int[] convert(Object[] objects) {
-        return null;
+        int[] ret = new int[objects.length];
+        for (int i = 0; i < objects.length; i++) {
+            Object o = objects[i];
+            if (o instanceof Number) {
+                ret[i] = ((Number) o).intValue();
+            }
+            if (o instanceof CharSequence) {
+                int intnum = 0;
+                CharSequence charSequence = (CharSequence) o;
+                for (int j = 0; j < charSequence.length(); j++) {
+                    int d = charSequence.charAt(j) - '0';
+                    intnum = intnum * 10 + d;
+                }
+                ret[i] = intnum;
+            }
+        }
+        return ret;
     }
 }
