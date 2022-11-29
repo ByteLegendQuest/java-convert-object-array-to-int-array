2be29f4c9d82b128ec6789318a9b33efff958d82
pamaomaoa <544060246@qq.com>
2022-11-29 15:25:10 +0000

    Update at 2022-11-29T15:25:10.649781Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Challenge {
     /**
      * Given an array containing any objects `Object[]`, convert it to `int[]` array. The objects
@@ -12,7 +15,30 @@ public class Challenge {
      *
      * <p>For example, input `[1, 2.0, 3.1, "4.999"]`, the output is `[1, 2, 3, 4]`.
      */
-    public static int[] convert(Object[] objects) {
-        return null;
+   public static int[] convert(Object[] objects) {
+        int len = objects.length;
+        int[] result = new int[len];
+        for (int x = 0; x < len; x++) {
+            if (objects[x] instanceof Number) {
+                if (objects[x] instanceof Integer) {
+                    result[x] = (int) objects[x];
+                } else if (objects[x] instanceof Double) {
+                    result[x] = ((Double) objects[x]).intValue();
+                } else if (objects[x] instanceof Float) {
+                    result[x] = ((Float) objects[x]).intValue();
+                } else if (objects[x] instanceof BigInteger) {
+                    result[x] = ((BigInteger) objects[x]).intValue();
+                } else if (objects[x] instanceof BigDecimal) {
+                    result[x] = ((BigDecimal) objects[x]).intValue();
+                }
+            } else {
+                if (objects[x] instanceof String)
+                    result[x] = Integer.valueOf((String) objects[x]);
+                else if (objects[x] instanceof StringBuilder)
+                    result[x] = Integer.valueOf(((StringBuilder) objects[x]).toString());
+
+            }
+        }
+        return result;
     }
 }
